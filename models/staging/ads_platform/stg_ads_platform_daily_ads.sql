fa949e3f741b5ace8299b21b3d6b8e280eedd62d
williamdst <schdwilliams@gmail.com>
2023-08-08 14:18:49 -0400

    test: Testing pre-commit pre-push

--- models/staging/ads_platform/stg_ads_platform_daily_ads.sql
+++ models/staging/ads_platform/stg_ads_platform_daily_ads.sql
@@ -8,8 +8,8 @@ renamed AS (
 
     SELECT
         date
-        ,campaign_id,
-        {{ dbt_utils.generate_surrogate_key(['date', 'campaign_id']) }} AS surrogate_key,
+        ,campaign_id
+        ,{{ dbt_utils.generate_surrogate_key(['date', 'campaign_id']) }} AS surrogate_key,
         spend AS campaign_spend,
         cpm AS cost_per_thousand_impressions,
         ctr AS campaign_click_through_rate
